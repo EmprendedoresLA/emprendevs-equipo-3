fcd282b1f7ad4cc8311a9437aa5ef817c74e8274
Pablo <pablocordoba11@gmail.com>
2015-11-14 16:57:39 -0300

    Cambio public menu

--- fixtures/dev/admins.js
+++ fixtures/dev/admins.js
@@ -1,5 +1,5 @@
 var actions = require('../../models/admins');
 
 module.exports.Admins = [
-{ name : 'superadmin', email : 'admin@anyandgo.com', password : '123456' }
+{ name : 'superadmin', email : 'coach@boostall.com', password : '123456' }
 ];
