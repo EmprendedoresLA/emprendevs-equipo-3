c001783b1d62e5780384663ecbadb9f8ff0ae750
Jacobo Dominguez <jacodom@MacBook-Pro-de-Jacobo.local>
2015-11-14 15:30:58 -0300

    agregados fixtures para dev

---
+++ fixtures/dev/trainings.js
@@ -0,0 +1,51 @@
+var actions = require('../../models/trainings');
+
+module.exports.Trainings = [
+  {
+    title: "Warming",
+    time: "09:00 PM",
+    date: "20/11/2015",
+    description: "Warmup before main training",
+    state: true,
+    groups: [],
+    activities: [
+        {
+          name : "Run",
+          description: "Running at a slow pace",
+          duration: 15 },
+        {
+          name: "Abs + Squats",
+          description:"4 series of abs (10 times)+ 5 series of squats without weight" ,
+          duration: 10 },
+        {
+          name:"Stretching" ,
+          description: "Procure stretch all the muscles involved",
+          duration: 5 }
+        ]
+  },
+  {
+    title: "Warming",
+    time: "15:00 PM",
+    date: "21/11/2015",
+    description: "Warmup before main training",
+    state: true,
+    groups: [],
+    activities: [
+        {
+          name : "Run",
+          description: "Running at a slow pace",
+          duration: 15
+        },
+        {
+          name: "Abs + Squats",
+          description:"4 series of abs (10 times)+ 5 series of squats without weight" ,
+          duration: 10
+        },
+        {
+          name:"Stretching" ,
+          description: "Procure stretch all the muscles involved",
+          duration: 5
+        }
+        ]
+  },
+];
