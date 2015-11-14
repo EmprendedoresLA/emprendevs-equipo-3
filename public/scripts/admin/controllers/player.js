264e4ef4453ad8ed496c72f9f8f7cb6554c517c9
Noel Cantoia <noel.cantoia@hotmail.com>
2015-11-14 19:22:42 -0300

    gestionar players, faltan botones

---
+++ public/scripts/admin/controllers/player.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name anyandgoApp.controller:playerCtrl
+ * @description
+ * # playerCtrl
+ * Controller of the anyandgoApp
+ */
+angular.module('anyandgoApp')
+  .controller('playerCtrl', function ($scope, Restangular) {
+   $scope.players = Restangular.all("players").getList().$object;
+
+  });
